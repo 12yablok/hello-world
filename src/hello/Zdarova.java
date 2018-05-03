86c3b229220b2cf50c7821a4b9c3c647043921e6
12yablok <q2e74@yandex.ru>
2018-05-03 12:37:36 +0300

    добавлен главный класс

--- src/hello/Zdarova.java
+++ src/hello/Zdarova.java
@@ -1,7 +1,14 @@
 package hello;
 
+import java.sql.SQLOutput;
+
 public class Zdarova {
+
+
     public static void main(String[] args) {
         System.out.println("Priva!");
+        System.out.println("hi Artem" +
+                "hello Artem" +
+                "hola Artem");
     }
 }
